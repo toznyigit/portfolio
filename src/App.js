76f172b85c0830c336ea0b34c29ad37f3adbaf10
toznyigit <t.oznygt+dev@gmail.com>
2023-10-31 23:04:03 +0300

    route and favicon

--- src/App.js
+++ src/App.js
@@ -14,11 +14,10 @@ function App() {
         <Router>
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
             <Route path='/portfolio' element={<Home />} />
-            <Route path='/experience' element={<Experience />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/project/:id' element={<ProjectDisplay />} />
+            <Route path='/portfolio/experience' element={<Experience />} />
+            <Route path='/portfolio/projects' element={<Projects />} />
+            <Route path='/portfolio/project/:id' element={<ProjectDisplay />} />
           </Routes>
           <Footer/>
         </Router>
