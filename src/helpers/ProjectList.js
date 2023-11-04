b4f1e7fe790af92699b6d5bbbd1ba5399f6eb7a7
toznyigit <t.oznygt+dev@gmail.com>
2023-11-04 20:36:16 +0300

    projects added

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -4,11 +4,11 @@ import SimulationImg from '../assets/projects/power_simulation/power.png'
 import SimulationInfo from '../assets/projects/power_simulation/info.json'
 import EcommerceImg from '../assets/projects/e_commerce/site.png'
 import EcommerceInfo from '../assets/projects/e_commerce/info.json'
-// import XDPImg from '../assets/projects/xdp/...'
+import XDPImg from '../assets/projects/xdp/xdp.png'
 import XDPInfo from '../assets/projects/xdp/info.json'
 import FAT32Img from '../assets/projects/fat32_navigation/VFAT_directory_entries.png'
 import FAT32Info from '../assets/projects/fat32_navigation/info.json'
-// import NNCImg from '../assets/projects/network_namespace_controller/...'
+import NNCImg from '../assets/projects/network_namespace_controller/nnc.png'
 import NNCInfo from '../assets/projects/network_namespace_controller/info.json'
 import RaytracerImg from '../assets/projects/raytracer/raytracer.png'
 import RaytracerInfo from '../assets/projects/raytracer/info.json'
@@ -39,7 +39,7 @@ export const projectList = [
     {
         "id": 3,
         ...XDPInfo,
-        //"image": XPDImg
+        "image": XPDImg
     },
     {
         "id": 4,
@@ -49,7 +49,7 @@ export const projectList = [
     {
         "id": 5,
         ...NNCInfo,
-        //"image": NNCImg
+        "image": NNCImg
     },
     {
         "id": 6,
